4535ba032f520e011816091687dc63d083379a28
OkazakiKouhei <kouhei@dreamly.se>
2024-03-22 17:12:55 +0900

    add google authentication

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,6 @@
+import authOptions from '@/libs/nextAuthOptions'
 import NextAuth from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
 
-const handler = NextAuth({
-  secret: process.env.NEXTAUTH_SECRET ?? '',
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
-    }),
-  ],
-})
+const handler = NextAuth(authOptions)
+
 export { handler as GET, handler as POST }
