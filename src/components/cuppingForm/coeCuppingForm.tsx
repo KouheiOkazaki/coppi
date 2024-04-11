be91b555d893f83e526a38701f561ffab2744f90
OkazakiKouhei <kouhei@dreamly.se>
2024-04-11 18:30:46 +0900

    refactor cuppingForm using map method

--- src/components/cuppingForm/coeCuppingForm.tsx
+++ src/components/cuppingForm/coeCuppingForm.tsx
@@ -28,7 +28,7 @@ const CoeCuppingForm: React.FC = () => {
             Sample No:
           </label>
           <input
-          id='sampleNo'
+            id="sampleNo"
             {...register('sampleNo', { required: true })}
             className="form-input px-4 py-2 border rounded-md"
           />
@@ -36,86 +36,24 @@ const CoeCuppingForm: React.FC = () => {
             <span className="text-red-500 text-sm">This field is required</span>
           )}
         </div>
-        <div className="flex flex-col">
-          <label htmlFor="cleanCup" className="block mb-2 font-medium">
-            Clean Cup
-          </label>
-          <select
-          id='cleanCup'
-            {...register('cleanCup', { required: true })}
-            className="form-select px-4 py-2 border rounded-md"
-          >
-            {scoreOptions.map((option) => (
-              <option key={option} value={option}>
-                {option}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="flex flex-col">
-          <label htmlFor="sweet" className="block mb-2 font-medium">
-            Sweet
-          </label>
-          <select
-          id='sweet'
-            {...register('sweet', { required: true })}
-            className="form-select px-4 py-2 border rounded-md"
-          >
-            {scoreOptions.map((option) => (
-              <option key={option} value={option}>
-                {option}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="flex flex-col">
-          <label htmlFor="acidity" className="block mb-2 font-medium">
-            Acidity
-          </label>
-          <select
-          id='acidity'
-            {...register('acidity', { required: true })}
-            className="form-select px-4 py-2 border rounded-md"
-          >
-            {scoreOptions.map((option) => (
-              <option key={option} value={option}>
-                {option}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="flex flex-col">
-          <label htmlFor="mouthfeel" className="block mb-2 font-medium">
-            Mouthfeel
-          </label>
-          <select
-          id='mouthfeel'
-            {...register('mouthfeel', { required: true })}
-            className="form-select px-4 py-2 border rounded-md"
-          >
-            {scoreOptions.map((option) => (
-              <option key={option} value={option}>
-                {option}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="flex flex-col">
-          <label htmlFor="flavor" className="block mb-2 font-medium">
-            Fravor
-          </label>
-          <select
-          id='flavor'
-            {...register('flavor', { required: true })}
-            className="form-select px-4 py-2 border rounded-md"
-          >
-            {scoreOptions.map((option) => (
-              <option key={option} value={option}>
-                {option}
-              </option>
-            ))}
-          </select>
-        </div>
+        {['cleanCup', 'sweet', 'acidity', 'mouthfeel', 'flavor'].map(fieldName => (
+          <div className="flex flex-col" key={fieldName}>
+            <label htmlFor={fieldName} className="block mb-2 font-medium">
+              {fieldName.charAt(0).toUpperCase() + fieldName.slice(1)}
+            </label>
+            <select
+              id={fieldName}
+              {...register(fieldName as keyof CoeFormData, { required: true })}
+              className="form-select px-4 py-2 border rounded-md"
+            >
+              {scoreOptions.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+          </div>
+        ))}
         <button
           type="submit"
           className="mt-4 px-4 py-2 bg-blue-600 text-white font-bold rounded hover:bg-blue-700 transition duration-200"
@@ -125,7 +63,7 @@ const CoeCuppingForm: React.FC = () => {
       </form>
       <TabNavigation />
     </>
-  )
-}
+  );
+};
 
-export default CoeCuppingForm
+export default CoeCuppingForm;
