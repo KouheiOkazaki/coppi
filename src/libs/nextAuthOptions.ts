8319895c855e923b30fcbe80c1ea51d13e506dca
OkazakiKouhei <kouhei@dreamly.se>
2024-04-14 22:56:09 +0900

    update tabnavigation CSS

--- src/libs/nextAuthOptions.ts
+++ src/libs/nextAuthOptions.ts
@@ -9,14 +9,5 @@ const authOptions: NextAuthOptions = {
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
 		}),
 	],
-	// Google認証を使用する場合、追加のページ設定はオプショナルです。
-	// 必要に応じてコメントアウトを解除し、カスタマイズしてください。
-	// pages: {
-	//   signIn: '/auth/signin',
-	//   signOut: '/auth/signout',
-	//   error: '/auth/error', // エラーコードはクエリ文字列で ?error= として渡されます
-	//   verifyRequest: '/auth/verify-request', // メール確認メッセージ用（メール送信がある場合）
-	//   newUser: '/auth/new-user' // 新規ユーザー登録ページ（オプショナル）
-	// }
 }
 export default authOptions
