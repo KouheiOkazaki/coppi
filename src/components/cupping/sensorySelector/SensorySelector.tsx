9ea53b860e8b01ff1b68d2afaa0bd82795c6d047
OkazakiKouhei <kouhei@dreamly.se>
2024-04-26 21:23:07 +0900

    add thumb label

--- src/components/cupping/sensorySelector/SensorySelector.tsx
+++ src/components/cupping/sensorySelector/SensorySelector.tsx
@@ -1,38 +1,43 @@
-import React, { useState } from 'react'
-import { scoreOptions } from '@/src/commons/const/const'
-import { SensorySelectorProps } from '@/src/commons/types/type'
+import React, { useState } from 'react';
+import { scoreOptions } from '@/src/commons/const/const';
+import { SensorySelectorProps } from '@/src/commons/types/type';
 
 export default function SensorySlider({ id, register }: SensorySelectorProps) {
-	const [selectedValue, setSelectedValue] = useState(scoreOptions[0])
+  const [selectedValue, setSelectedValue] = useState(scoreOptions[0]);
 
-	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		const newValue = parseFloat(event.target.value)
-		if (![0.5, 1.5, 2.5, 3.5, 4.5, 5.5].includes(newValue)) {
-			setSelectedValue(newValue)
-		}
-	}
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = parseFloat(event.target.value);
+    if (![0.5, 1.5, 2.5, 3.5, 4.5, 5.5].includes(newValue)) {
+      setSelectedValue(newValue);
+    }
+  };
 
-	return (
-		<div className="flex flex-col">
-			<input
-				type="range"
-				id={id}
-				{...register(id, { required: true })}
-				className="form-range"
-				min={scoreOptions[0]} // 最小値をscoreOptionsの最初の値に設定
-				max={scoreOptions[scoreOptions.length - 1]} // 最大値をscoreOptionsの最後の値に設定
-				step="0.5" // ステップを適切な値に設定
-				list="tickmarks" // スライダーの値のリストを使用する
-				value={selectedValue} // 選択された値を表示
-				onChange={handleChange} // 値が変更された時の処理
-			/>
-			<datalist id="tickmarks">
-				{' '}
-				{/* リストを作成 */}
-				{scoreOptions.map((option) => (
-					<option key={option} value={option} />
-				))}
-			</datalist>
-		</div>
-	)
+  // 値に基づいてdiv要素のスタイルを動的に変更する
+  const divStyle = {
+    marginLeft: `${selectedValue! * 12}%`, // サムの値に応じて左マージンを調整する
+    transition: 'margin-left 0.2s ease', // マージンの変化を滑らかにする
+  };
+
+  return (
+    <div className="flex flex-col">
+      <div style={divStyle}>{selectedValue}</div>
+      <input
+        type="range"
+        id={id}
+        {...register(id, { required: true })}
+        className="form-range"
+        min={scoreOptions[0]}
+        max={scoreOptions[scoreOptions.length - 1]}
+        step="0.5"
+        list="tickmarks"
+        value={selectedValue}
+        onChange={handleChange}
+      />
+      <datalist id="tickmarks">
+        {scoreOptions.map((option) => (
+          <option key={option} value={option} />
+        ))}
+      </datalist>
+    </div>
+  );
 }
