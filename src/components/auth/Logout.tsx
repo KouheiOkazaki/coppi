96970f2197714bfbd784bc80eabb451868e8845c
OkazakiKouhei <kouhei@dreamly.se>
2024-05-12 23:45:01 +0900

    update auth UI

--- src/components/auth/Logout.tsx
+++ src/components/auth/Logout.tsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import { useSession, signOut } from 'next-auth/react'
+import { signOut, useSession } from 'next-auth/react'
 
 export default function Logout() {
-	const { status } = useSession()
+  const { status } = useSession()
 
-	if (status === 'authenticated') {
-		return (
-			<div>
-				<button onClick={() => signOut()}>ログアウト</button>
-			</div>
-		)
-	}
-	return null
+  if (status === 'authenticated') {
+    return (
+      <div>
+        <button
+          onClick={() => signOut()}
+          className="border border-red-500 hover:bg-red-100 text-red-500 py-2 px-4 rounded"
+        >
+          ログアウト
+        </button>
+      </div>
+    )
+  }
+  return null
 }
